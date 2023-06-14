76eaf1c834ec66f52a2bca6f76f7ebe8fb3af8a0
orelmor <orelm86@gmail.com>
2023-06-14 14:49:04 +0300

    Register form compelete plus adding notification service

--- frondend/src/Components/components/AuthArea/Register/Register.tsx
+++ frondend/src/Components/components/AuthArea/Register/Register.tsx
@@ -1,21 +1,41 @@
+import { useForm } from "react-hook-form";
 import "./Register.css";
+import UserModel from "../../../../Models/UserModel";
+import authService from "../../../../Services/AuthService";
+import { useNavigate } from "react-router-dom";
+import notificationService from "../../../../Services/NotificationService";
 
 function Register(): JSX.Element {
+
+    const {register , handleSubmit} = useForm<UserModel>()
+    const navigate = useNavigate()
+
+    async function send(user:UserModel){
+        try {   
+            await authService.register(user)
+            notificationService.succees(`Wellcome ${user.firstName}`)
+             navigate("/vacationList")
+            
+        } catch (err:any) {
+            notificationService.error(err)
+        }
+    }
+
     return (
         <div className="Register">
 			<h2>Register</h2>
-            <form>
+            <form onSubmit={handleSubmit(send)}>
                 <label >First name</label>
-                <input type="text" />
+                <input type="text"  {...register("firstName")} required minLength={2} maxLength={25}/>
 
-                <label >Last name name</label>
-                <input type="text" />
+                <label >Last name</label>
+                <input type="text" {...register("lastName")} required minLength={2} maxLength={25}/>
 
                 <label>Email</label>
-                <input type="email"/>
+                <input type="email" {...register("email")} required maxLength={50}/>
 
                 <label>Password</label>
-                <input type="password"  />
+                <input type="password" {...register("password")} required minLength={4} maxLength={25} />
                 <button type="submit">Register</button>
             </form>
         </div>
---
+++ frondend/src/Services/NotificationService.ts
@@ -0,0 +1,27 @@
+import { Notyf } from "notyf"
+
+class NotificationService{
+    private notification = new Notyf({
+        duration: 2500, position: { x: "center", y: "top" }, types: [{ type: 'info', background: 'blue',icon: "Feedback"}]})
+
+    public succees(message: string): void {
+        this.notification.success(message)
+    }
+
+    public error(err: any): void {
+        const message = this.checkMessageType(err)
+        this.notification.error(message)
+    }
+
+    public checkMessageType(err: any): string {
+        if (typeof err === "string") return err
+        if (typeof err.response?.data === "string") return err.response.data
+        if (Array.isArray(err.response?.data)) return err.response.data[0]
+        if (typeof err.message === "string") return err.message
+        return "Some error ocurred, Please try again"
+    }
+}
+
+
+const notificationService = new NotificationService()
+export default notificationService
